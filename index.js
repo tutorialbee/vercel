ce9fa72e4d2d02849603ff1977c2c3a40bd1f40f
muthiah <tutorialbees@gmail.com>
2023-05-08 08:34:08 +0530

    try7

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
 
 
-//app.get('/', (req, res) => res.send('Home 2 Page Route'));
+app.get('/', (req, res) => res.send('Home 2 Page Route'));
 
-app.get('/', (req, res) => {
-     res.sendFile('./views/index.html', {root : __dirname});
-});
+// app.get('/', (req, res) => {
+//     res.sendFile('./views/index.html', {root:__dirname});
+// });
 
 app.get('/about', (req, res) => res.send('About Page Route'));
 
